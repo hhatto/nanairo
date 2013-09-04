870700de6b3e8e50511deb4df9c036bd0e37c226
Hideo Hattori <hhatto.jp@gmail.com>
2013-09-05 02:38:45 +0900

    implement named color

--- color_test.go
+++ color_test.go
@@ -13,6 +13,10 @@ func TestFgColorHtml6(t *testing.T) {
     fmt.Println(FgColor("#f19e30", "Hello World"))
 }
 
+func TestFgColorHtmlName(t *testing.T) {
+    fmt.Println(FgColor("limegreen", "Hello World"))
+}
+
 func TestBgColor(t *testing.T) {
     fmt.Println(BgColor("#f93", "Hello World"))
 }
